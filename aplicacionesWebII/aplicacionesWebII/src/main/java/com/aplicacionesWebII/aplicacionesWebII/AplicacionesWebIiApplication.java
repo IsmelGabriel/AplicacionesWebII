d57e327173617c807e20c3dd7266d57d82e7adff
IsmelGabriel <ssismel28@gmail.com>
2025-08-10 10:59:26 -0500

    first commit

---
+++ aplicacionesWebII/aplicacionesWebII/src/main/java/com/aplicacionesWebII/aplicacionesWebII/AplicacionesWebIiApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacionesWebII.aplicacionesWebII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionesWebIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionesWebIiApplication.class, args);
+	}
+
+}
---
+++ aplicacionesWebII/aplicacionesWebII/src/test/java/com/aplicacionesWebII/aplicacionesWebII/AplicacionesWebIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacionesWebII.aplicacionesWebII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionesWebIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
